ae1bc1fdc6bfc485ad4d2e81ccfced9fe04544db
albertteoh <see.kwang.teoh@gmail.com>
2022-02-17 22:47:12 +1100

    Cleanup

--- inventory-frontend/main.go
+++ inventory-frontend/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/albertteoh/gin-example/data"
 	"github.com/gin-gonic/gin"
-	resty "github.com/go-resty/resty/v2"
+	"github.com/go-resty/resty/v2"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
